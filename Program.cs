0da406bfe374e5b7af71e3d95e393644a02b5741
Peng, Yicheng <yicheng.peng@siemens.com>
2019-04-22 18:01:55 +0800

    initial learn dotnet core task

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // getWorld("Yicheng, ");
+            await call();
+        }
+
+        public static string getWorld(string name) => getHello(name);
+        public static string getHello(string name)
+        {
+            string nameStr = name + " hahaha!!!";
+            Console.WriteLine("getHello: " + nameStr);
+            return nameStr;
+        }
+
+        public static async Task<string> call()
+        {
+            Console.WriteLine("----------->1");
+            int s = await foo();
+            Console.WriteLine("----------->2");
+            return "1";
+        }
+
+        public static async Task<int> foo()
+        {
+            Console.WriteLine("----------->3");
+            await Task.Delay(500);
+            Console.WriteLine("----------->4");
+            return 1;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+### Reference
+https://www.jianshu.com/p/4768c954a85f
+https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
